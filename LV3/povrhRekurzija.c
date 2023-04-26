6a84e9215fb73a3d7a96cd3f5bd9e626388662bf
Dominik Župarić <107096417+DZuparic@users.noreply.github.com>
2023-04-26 18:26:14 +0200

    Add files via upload

---
+++ LV3/povrhRekurzija.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int povrh(int a, int b) {
+    if (a == b || b == 0) return 1;
+    return povrh(a - 1, b - 1) + povrh(a - 1, b);
+}
+
+
+
+int main() {
+
+    int a = 10;
+    int b = 5;
+
+    printf("%d\n", povrh(a, b));
+
+    return 0;
+}
